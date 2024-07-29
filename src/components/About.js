54600ef0a52bb88dc6e763c43ab2600d3dedee6a
deshankbisen <63210848+deshankbisen@users.noreply.github.com>
2024-07-29 09:25:11 +0000

    CHanged:

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const PanditjiDashboard = () => (
+    <div>
+        <h1>Panditji Dashboard</h1>
+        <p>Welcome to your Panditji dashboard.</p>
+    </div>
+);
+
+export default PanditjiDashboard;
---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ProfilePage = () => (
+    <div>
+        <h1>User Profile Page</h1>
+        <p>Welcome to your profile page.</p>
+    </div>
+);
+
+export default ProfilePage;
